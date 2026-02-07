879f3fbb37d40163d0ab67b4072d59f8c2478751
Clayton Stallings <crstall2004@gmail.com>
2026-02-06 17:30:01 -0700

    making more progress on the make move function

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -121,6 +121,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
