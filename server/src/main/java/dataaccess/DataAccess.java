b18bd611d10a8fbbfc1ebc1bf63ccf2396ece3fd
Clayton Stallings <crstall2004@gmail.com>
2026-03-02 12:56:48 -0700

    Trying to create data access interface that DAO's will implement

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,21 @@
+package dataaccess;
+
+
+import model.*;
+
+public interface DataAccess {
+    void clear() throws DataAccessException;
+
+
+
+//    Pet addPet(Pet pet) throws DataAccessException;
+//
+//    PetList listPets() throws DataAccessException;
+//
+//    Pet getPet(int id) throws DataAccessException;
+//
+//    void deletePet(Integer id) throws DataAccessException;
+//
+//    void deleteAllPets() throws DataAccessException;
+}
+
