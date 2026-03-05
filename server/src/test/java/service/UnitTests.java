80e919365c1baad7b4d931bbde1799274d80c196
Clayton Stallings <crstall2004@gmail.com>
2026-03-04 22:55:52 -0700

    finished putting asserts in all the unit tests. we should be chilling at this point...

--- server/src/test/java/service/UnitTests.java
+++ server/src/test/java/service/UnitTests.java
@@ -24,17 +24,21 @@ class UnitTests {
     GameDAO gameDAO = new MemoryGameDAO();
     AuthDAO authDAO = new MemoryAuthDAO();
 
+    UserService testService = new UserService(userDAO, authDAO, gameDAO);
 
     LoginService loginService = new LoginService(userDAO, authDAO);
     RegisterService registerService = new RegisterService(userDAO,loginService);
     LogoutService logoutService = new LogoutService(authDAO);
     ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
-    UserService testService = new UserService(userDAO, authDAO, gameDAO);
     CreateGameService createGameService = new CreateGameService(gameDAO,authDAO);
+    ListGamesService listGamesService = new ListGamesService(gameDAO,authDAO);
+    JoinGameService joinGameService = new JoinGameService(gameDAO,authDAO);
 
     @BeforeEach
     void clear() throws ResponseException {
         clearService.clearAllDatabases();
+        var users = testService.getUsers();
+        assertEquals(0, users.size());
     }
 
     //test clear, by registering someone, clearing the DBs, and then seeing if the users list is 1 or 0
@@ -91,12 +95,14 @@ class UnitTests {
         assertEquals(401, exception.getStatusCode());
     }
 
-    //register and log out without errors
+    //register and log out, AND THEN try to use auth token (list games) that you already logged out of.
     @Test
     void logoutGood() throws ResponseException {
         var user = new UserData("brian", "password123", "brian@gmail.com");
         var auth = registerService.registerUser(user);
         logoutService.logoutUser(auth.authToken());
+        ResponseException exception = assertThrows(ResponseException.class, () -> logoutService.logoutUser(auth.authToken()));
+        assertEquals(401, exception.getStatusCode());
     }
 
     //tries to log out 2 times
@@ -109,7 +115,7 @@ class UnitTests {
         assertEquals(401, exception.getStatusCode());
     }
 
-    //register and use auth token to create game
+    //create 2 games, make sure the amount of games is actually 2.
     @Test
     void createGameGood() throws ResponseException {
         var user = new UserData("brian", "password123", "brian@gmail.com");
@@ -117,6 +123,9 @@ class UnitTests {
         GameData game = new GameData(4,null, null,"this is the name", null);
 
         createGameService.createGame(auth.authToken(),game);
+        createGameService.createGame(auth.authToken(),new GameData(5,null, null,"ASDF", null));
+        var games = listGamesService.listGames(auth.authToken(), null);
+        assertEquals(2, games.size());
     }
 
     //tries to create game with a bad token...
@@ -127,6 +136,49 @@ class UnitTests {
         GameData game = new GameData(4,null, null,null, null);
         ResponseException exception = assertThrows(ResponseException.class, () -> createGameService.createGame("fake auth token",game));
         assertEquals(401, exception.getStatusCode());
+    }
+
+    //creates a game, then lists games and sees if the size of them increased by one.
+    @Test
+    void listGamesGood() throws ResponseException {
+        var user = new UserData("brian", "password123", "brian@gmail.com");
+        var auth = registerService.registerUser(user);
+        GameData game = new GameData(4,null, null,"this is the name", null);
+        createGameService.createGame(auth.authToken(), game);
+        var games = listGamesService.listGames(auth.authToken(), null);
+        assertEquals(1, games.size());
+    }
+
+    //tries to list games with a bad token
+    @Test
+    void listGamesBad() throws ResponseException{
+        ResponseException exception = assertThrows(ResponseException.class, () -> listGamesService.listGames("fake token", null));
+        assertEquals(401, exception.getStatusCode());
+    }
+
+    //join game like normal
+    @Test
+    void joinGameGood() throws ResponseException {
+        var user = new UserData("brian", "password123", "brian@gmail.com");
+        var auth = registerService.registerUser(user);
+        GameData game = new GameData(1,null, null,"this is the name", null);
+        createGameService.createGame(auth.authToken(), game);
+        assertDoesNotThrow(() -> joinGameService.joinGame(auth.authToken(), "WHITE", 1));
+        
+    }
+
+    //has 2 players join 1 game, but as the same color.
+    @Test
+    void joinGameBad() throws ResponseException{
+        var user1 = new UserData("brian", "password123", "brian@gmail.com");
+        var auth1 = registerService.registerUser(user1);
+        var user2 = new UserData("charles", "passwasdaord123", "charles@gmail.com");
+        var auth2 = registerService.registerUser(user2);
+        GameData game = new GameData(1,null, null,"this is the name", null);
+        createGameService.createGame(auth1.authToken(), game);
+        joinGameService.joinGame(auth1.authToken(), "WHITE", 1);
+        ResponseException exception = assertThrows(ResponseException.class, () -> joinGameService.joinGame(auth2.authToken(), "WHITE", 1));
+        assertEquals(403, exception.getStatusCode());
     }
 
 }
