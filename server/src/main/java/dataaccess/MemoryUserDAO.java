7325a9992139f7fa6abcfad1c0aa9ba4445d3d12
Clayton Stallings <crstall2004@gmail.com>
2026-03-04 18:56:21 -0700

    passed the Re-Register User test.

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -10,8 +10,12 @@ public class MemoryUserDAO implements UserDAO {
     final private HashMap<Integer, UserData> users = new HashMap<>();
 
     @Override
-    public UserData createUser(UserData user) {
+    public UserData createUser(UserData user) throws ResponseException {
         user = new UserData(user.username(), user.password(), user.email());
+
+        if(users.get(user.username().hashCode()) != null){
+            throw new ResponseException(403, "Error: Forbidden. User already registered.");
+        }
 
         users.put(user.username().hashCode(), user);
         return user;
