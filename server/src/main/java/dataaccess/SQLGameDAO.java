01d36eda3b214ed1b293710e72610ce52bb51ba2
Clayton Stallings <crstall2004@gmail.com>
2026-03-11 22:10:24 -0600

    Got all the StandardAPITests working again!!!

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -32,10 +32,10 @@ public class SQLGameDAO implements GameDAO {
 
     @Override
     public GameData createGame(GameData game) throws ResponseException {
-        int fakeid = new Random().nextInt();
+        int fakeid = new Random().nextInt(10000) + 10001;
         GameData fakeGame = new GameData(fakeid, null, null, game.gameName(), new ChessGame());
         String gameJson = new Gson().toJson(fakeGame);
-        String statement = "INSERT INTO user (fakeid, data) VALUES (?, ?)";
+        String statement = "INSERT INTO games (gameID, data) VALUES (?, ?)";
         int gameID = executeUpdateWithInt(statement, fakeid, gameJson);
         GameData finalGame = new GameData(gameID, null, null, game.gameName(), fakeGame.game());
         String finalJson = new Gson().toJson(finalGame);
@@ -47,7 +47,7 @@ public class SQLGameDAO implements GameDAO {
     @Override
     public HashMap<Integer, GameData> listGames() throws ResponseException {
         HashMap<Integer, GameData> games = new HashMap<>();
-        String statement = "SELECT data FROM games";
+        String statement = "SELECT gameID, data FROM games";
         try (Connection conn = DatabaseManager.getConnection()) {
             try (var ps = conn.prepareStatement(statement)) {
                 try (var rs = ps.executeQuery()) {
@@ -64,9 +64,51 @@ public class SQLGameDAO implements GameDAO {
         return games;
     }
 
+    public GameData getGame(int gameID) throws ResponseException {
+        String statement = "SELECT data FROM games WHERE gameID = ?";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String json = rs.getString("data");
+                        return new Gson().fromJson(json, GameData.class);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("Error retrieving users: %s", e.getMessage()));
+        }
+        return null;
+    }
+
     @Override
     public void joinGame(String playerColor, int gameId, String username) throws ResponseException{
-
+        GameData game = getGame(gameId);
+        if(game == null){
+            throw new ResponseException(400, "Error: Bad Request. That GameId does not exist");
+        }
+        if(!Objects.equals(playerColor, "WHITE") && !Objects.equals(playerColor, "BLACK")){
+            throw new ResponseException(400, "Error: Bad Request. Choose WHITE or BLACK for team color.");
+        }
+        if(playerColor.equals("WHITE")){
+            if(game.whiteUsername() != null){
+                throw new ResponseException(403, "Error: already taken. That game already has a white player.");
+            }
+            GameData newGame = new GameData(game.gameID(),username,game.blackUsername(),game.gameName(),game.game());
+            String newJson = new Gson().toJson(newGame);
+            String updateStatement = "UPDATE games SET data = ? WHERE gameID = ?";
+            executeUpdate(updateStatement, newJson, newGame.gameID());
+        }
+        if(playerColor.equals("BLACK")){
+            if(game.blackUsername() != null){
+                throw new ResponseException(403, "Error: already taken. That game already has a black player.");
+            }
+            GameData newGame = new GameData(game.gameID(),game.whiteUsername(),username,game.gameName(),game.game());
+            String newJson = new Gson().toJson(newGame);
+            String updateStatement = "UPDATE games SET data = ? WHERE gameID = ?";
+            executeUpdate(updateStatement, newJson, newGame.gameID());
+        }
     }
 
 
